d855fee97bb483fc7b9d7d1eb6286ef31279d77b
Marc Kohaupt <debuglevel@gmail.com>
2019-03-18 12:29:05 +0100

    Fix typo

--- examples/curl/upload.sh
+++ examples/curl/upload.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # This script uploads the upload.json
 # An UUID will be printed to stdout. The upload and download script can be chained therefore:
-# $ bash latex-microservice-download.sh $(bash latex-microservice-uplod.sh)
+# $ bash latex-microservice-download.sh $(bash latex-microservice-upload.sh)
 curl -X POST -d @upload.json -H "Content-Type: application/json" -H "Accept: application/json" http://localhost/analyses/
